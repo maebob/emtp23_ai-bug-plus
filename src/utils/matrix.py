c03c5f7d723c56473bbc46d08ce0e7639dc7a90e
Miss Sophie <95981851+misssophieexplores@users.noreply.github.com>
2023-01-11 16:18:19 +0100

    Update matrix.py
    takes in either controlflow or dataflow matrix to determine the number of bugs used

--- src/utils/matrix.py
+++ src/utils/matrix.py
@@ -1,27 +1,9 @@
 import numpy as np
 
-"""
-Define how many positions are taken up in the matrix/array for the learner
-at the beginning and end.
-In order to count the number of positions in the matrix correctly, we need to count from 1 and not from zero.
-Therefore, use for EXTRAS_START the number of positions minus 1. 
-"""
-# TODO: discuss!
-EXTRAS_START = 2 # The first three positions (0, 1, and 2) are taken for up, down, out
-EXTRAS_END = 1 # The last position is taken for one deleted edge. Change if we want higher (but fixed) number of reserved positions for the solution.
-
 def number_bugs(matrix_or_array) -> int:
-    # TODO: Should we take the number of extra positions as an input or rather keep them as fixed variables?
+
     """
-    Given the matrix or flattened matrix of control- and dataflow, returns the number of bugs.
-
-    In more detail:
-
-    Takes in an matrix or array consiting of the input pair and output, the controlflow matrix flattened,
-    the dataflow matrix flattened and lastly, the position of the deleted edge.
-    1) Flattens the matrix. If the input is an array already, nothing changes -> array
-    2) Calculates the number of elements in each matrix -> no_fields
-    3) Calculates the number of bugs by solving the quadratic equation, see explanation below.
+    Given the matrix or flattened matrix of the controlflow matrix (or dataflow matrix), returns the number of bugs.
 
     n = number of bugs
     Controlflow matrix: rows from 0 to n+1, columns from 0 to 2n
@@ -35,54 +17,65 @@ def number_bugs(matrix_or_array) -> int:
     <=> 0 = 2n^2+5n+(2-no_fields)
     """
     array = matrix_or_array.flatten()
-    no_fields = int((array.size - EXTRAS_START - EXTRAS_END)/2) # number of fields in each matrix
-    no_bugs = int((-5 + (25-8*(2 - no_fields))**(0.5))/4) # number of bugs used 
-    print('no_bugs: ', no_bugs)
+    no_bugs = int((-5 + (25-8*(2 - array.size))**(0.5))/4) # number of bugs used to form the matrix
 
     return no_bugs
 
 
+
+"""
+# Change if needed:
+# Define how many positions are taken up in the matrix/array for the learner
+# at the beginning and end.
+# In order to count the number of positions in the matrix correctly, we need to count from 1 and not from zero.
+# Therefore, use for EXTRAS_START the number of positions minus 1. 
+
+EXTRAS_START = 3
+EXTRAS_END = 1
+
 def array_to_matrix(array) -> np.array:
-    """
-    reshape the array into controlflow and dataflow matrices.
-    n = number of bugs, #X = number of X
-    dataflow: 
-        [Wdh LA: (m x n)-matrix mit m Zeilen und n Spalten]
-        #rows = n+2
-        #columns = 2n+1
-        shape: (n+2, 2n+1)
-        start position: EXTRAS_START
-        end position: EXTRAS_START + ((n+2)*(2n+1))
-    controlflow:
-        #rows = 2n+1
-        #columns = n+2
-        shape: (2n+1, n+2)
-        start position: EXTRAS_START + ((n+2)*(2n+1))
-        end position:  EXTRAS_START + 2* ((n+2)*(2n+1))
-    """
-    no_bugs = number_bugs(array)
+    
+    #reshape the array into controlflow and dataflow matrices.
+    #n = number of bugs, #X = number of X
+    #dataflow: 
+    #    [Wdh LA: (m x n)-matrix mit m Zeilen und n Spalten]
+    #    #rows = n+2
+    #    #columns = 2n+1
+    #    shape: (n+2, 2n+1)
+    #    start position: EXTRAS_START
+    #    end position: EXTRAS_START + ((n+2)*(2n+1))
+    #controlflow:
+    #    #rows = 2n+1
+    #    #columns = n+2
+    #    shape: (2n+1, n+2)
+    #    start position: EXTRAS_START + ((n+2)*(2n+1))
+    #    end position:  EXTRAS_START + 2* ((n+2)*(2n+1))
+    
+    # workaround to determine the number of bugs by finding the first matrix:
+    no_extra_array = np.array(array[EXTRAS_START : -EXTRAS_END])
+    no_fields = no_extra_array.size
+    first_matrix_flatten = no_extra_array[0 : int(no_fields/2)]
+
+    no_bugs = number_bugs(first_matrix_flatten)
+
     controlflow = array[EXTRAS_START : EXTRAS_START +(no_bugs + 2) * (2 * no_bugs + 1)].reshape(no_bugs + 2, 2 * no_bugs + 1)
     dataflow = array[EXTRAS_START +(no_bugs + 2) * (2 * no_bugs + 1) : EXTRAS_START + 2 * ((no_bugs + 2) * (2 * no_bugs + 1))].reshape(2 * no_bugs + 1, no_bugs + 2)
     learner_input = array[0:EXTRAS_START]
     missing_positions = array[-EXTRAS_END:]
-    # TODO: includes up, down, out, missing positions; okay or do we want to change it?
-    # TODO: discuss shape of "input" matrix for environment; does not work as is atm
-    matrix = np.array([learner_input, controlflow, dataflow, missing_positions])
-    print('matrix: ', matrix)
-    print('learner_input: ', learner_input)
-    print('missing_positions', missing_positions)
-    print('controlflow: ', controlflow)
-    print('dataflow: ', dataflow)
+    matrix = [controlflow, dataflow]
     return matrix
-
-# """
+"""
+"""
 def main():
-    test_array = np.array([3, 5, 8,
+    test_array_with_extras = np.array(
+    [3, 5, 8,
+
     0, 1, 0, 0, 1, 0, 1, 
     0, 0, 0, 0, 0, 0, 0, 
     0, 0, 0, 0, 0, 0, 0, 
     0, 0, 0, 0, 0, 0, 0, 
     0, 1, 0, 0, 0, 0, 1, 
+
     0, 0, 0, 1, 0, 
     0, 0, 0, 1, 0, 
     0, 0, 0, 0, 0, 
@@ -90,18 +83,28 @@ def main():
     0, 0, 0, 1, 0, 
     0, 0, 0, 0, 0, 
     0, 0, 0, 0, 1,
+
     12])
 
-    #array_to_matrix(test_array)
-    number_bugs(test_array)
-    #number_bugs(array_to_matrix(test_array))
+    test_array = np.array([0, 1, 0, 0, 1, 0, 1, 
+    0, 0, 0, 0, 0, 0, 0, 
+    0, 0, 0, 0, 0, 0, 0, 
+    0, 0, 0, 0, 0, 0, 0, 
+    0, 1, 0, 0, 0, 0, 1])
+    test_matrix = np.reshape(test_array, (5, 7))
+
+
+
+
+    array_to_matrix(test_array_with_extras)
+
 
 
 if __name__ == "__main__":
     main()
 
 
- # """
+ """
 
 """
 # Taken from env.observation_space
