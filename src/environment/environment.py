46f40f4c431b972857bd0ae93b9b504d5a99345d
Miss Sophie <95981851+misssophieexplores@users.noreply.github.com>
2023-01-21 17:51:53 +0100

    added sys.paths for all our machines

--- src/environment/environment.py
+++ src/environment/environment.py
@@ -1,7 +1,9 @@
 from gym import Env, spaces
 import numpy as np
 import sys
-sys.path.append('C:/Users/D073576/Documents/GitHub/BugPlusEngine/')
+sys.path.append('/Users/mayte/github/bugplusengine') # Mayte
+# sys.path.append('C:/Users/D073576/Documents/GitHub/BugPlusEngine/') # Mae
+# sys.path.append('/Users/aaronsteiner/Documents/GitHub/BugPlusEngine/') # Aaron
 from src.translation.matrix_to_json import main as matrix_to_json
 from src.engine.eval import main as eval_engine
 
