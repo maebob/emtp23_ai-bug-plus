0a362ee1eb3438a2f3d0cc318b2603ade723330e
Aaron9812 <79525803+Aaron9812@users.noreply.github.com>
2023-02-07 19:20:05 +0100

    new configs

--- src/generator/generate_random_configs.py
+++ src/generator/generate_random_configs.py
@@ -76,7 +76,7 @@ if __name__ == "__main__":
         # delete i edges from the control matrix
         deleted_control_matrices = delete_edges_in_matrix(control_matrix, 1, np.array([i]))
         # generate a config with the input pairs
-        config = generate_config_with_input_pairs(control_matrix, data_matrix, range(0, 10), lambda x, y: x + y)
+        config = generate_config_with_input_pairs(control_matrix, data_matrix, range(0, 10), lambda x, y: x + 1)
         
         # add the config to the configs
         if configs.size == 0:
@@ -85,4 +85,4 @@ if __name__ == "__main__":
             configs = np.concatenate((configs, config))
 
     # save the configs to a file
-    np.savetxt("configs.csv", configs, delimiter=";", fmt="%d")
+    np.savetxt("configs_0207.csv", configs, delimiter=";", fmt="%d")
