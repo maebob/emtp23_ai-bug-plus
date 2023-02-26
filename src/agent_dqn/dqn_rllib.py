d8e613ac054d622388597746a17feeb4d3acbd42
Miss Sophie <95981851+misssophieexplores@users.noreply.github.com>
2023-02-26 19:58:54 +0100

    Create dqn_rllib.py

---
+++ src/agent_dqn/dqn_rllib.py
@@ -0,0 +1,69 @@
+import gym
+import math
+import random
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+
+from collections import namedtuple, deque
+from itertools import count
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+
+
+import pandas as pd
+
+import sys
+import os
+from dotenv import load_dotenv
+
+# load the .env file
+load_dotenv()
+# append the absolute_project_path from .env variable to the sys.path
+sys.path.append(os.environ.get('absolute_project_path'))
+from src.environment import environment_ray as environment
+from src.utils.matrix import number_bugs, array_to_matrices
+
+
+import ray
+from ray import tune
+from ray.rllib.algorithms.dqn import DQNConfig
+
+
+#RLlib:
+# config
+# build
+# train
+# evaluate
+
+#TODO: implement method to apply the strategies
+# choose between two modi:
+# config_strategy = 'priority' # 'random' or 'priority'
+# problem_solving_strategy = 'reload' # 'reload' or 'new'
+
+
+#RLlib:
+# config
+# build
+# train
+# evaluate
+ray.init()
+config = (  # 1. Configure the algorithm,
+    DQNConfig()
+    .environment(environment.BugPlus)
+    .rollouts(num_rollout_workers=2)
+    .framework("tf2")
+    .training(model={"fcnet_hiddens": [64, 64]})
+    .evaluation(evaluation_num_workers=1)
+)
+
+algo = config.build()  # 2. build the algorithm,
+
+for _ in range(5):
+    print(algo.train())  # 3. train it,
+
+algo.evaluate()  # 4. and evaluate it.
+
