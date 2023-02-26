fe522b7ccb512bbbb42acd2e38d4e9189980bebf
Miss Sophie <95981851+misssophieexplores@users.noreply.github.com>
2023-02-26 20:01:31 +0100

    Create test.py
    testing the gym environment for ray

---
+++ src/environment/test.py
@@ -0,0 +1,9 @@
+import ray
+from ray.rllib.utils import check_env
+import environment_ray
+# print(type(environment_ray.BugPlus))
+check_env(environment_ray.BugPlus())
+"""
+ValueError: Env must be of one of the following supported types: BaseEnv, gymnasium.Env, gym.Env, MultiAgentEnv, VectorEnv, RemoteBaseEnv, ExternalMultiAgentEnv, ExternalEnv, but instead is of type <class 'list'>.
+"""
+
