b826b8e3b96fbc31538f9a3cd511bca15898928a
Miss Sophie <95981851+misssophieexplores@users.noreply.github.com>
2023-03-04 10:38:43 +0100

    Update test_env_ray.py

--- src/environment/test_env_ray.py
+++ src/environment/test_env_ray.py
@@ -1,4 +1,4 @@
 import ray
 from ray.rllib.utils import check_env
-import environment_ray
-check_env(environment_ray.BugPlus()) # checks the custom environment for errors
+import environment_ray_functioning
+check_env(environment_ray_functioning.BugPlus()) # checks the custom environment for errors
