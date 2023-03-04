91d10415d87b4ae718305c7391571e5eaa25d6e9
Aaron9812 <aaronsteiner79@gmail.com>
2023-03-04 18:35:06 +0100

    Added correct wand config

--- src/agent_dqn/dqn_tune_rllib.py
+++ src/agent_dqn/dqn_tune_rllib.py
@@ -14,7 +14,6 @@ load_dotenv()
 # append the absolute_project_path from .env variable to the sys.path
 sys.path.append(os.environ.get('absolute_project_path'))
 # from src.environment import environment_ray as environment
-from src.environment.env_complex_observation import BugPlus
 
 
 # clear the terminal
@@ -28,34 +27,34 @@ stop = {
 }
 tune.stopper.ExperimentPlateauStopper(
     metric="episode_reward_mean",
-    std=0.01,
+    std=1.0,
     top=100,
     mode="max",
     patience=10,
 )
+from src.environment.env_complex_observation import BugPlus
 
-tune.run("DQN",
-         config={"env": BugPlus,
-                 "seed": 42069,
-                 "framework": "torch",
-                 "num_workers": # TODO: anpassen! 2,
-                 "num_gpus": 0,
-                 "num_envs_per_worker": 1,
-                 "num_cpus_per_worker": 1,
-                 },
+tune.run("DQN", 
+        config={"env": BugPlus,
+            "seed": 42069,
+            "framework": "torch",
+            "num_workers": 20, # TODO: anpassen! 2,
+            "num_gpus": 0,
+            "num_envs_per_worker": 1,
+            "num_cpus_per_worker": 1,
+        },
          local_dir="result_data/",
          callbacks=[
-             # adjust the entries here to conform to your wandb environment
-             # cf. https://docs.wandb.ai/ and https://docs.ray.io/en/master/tune/examples/tune-wandb.html
              WandbLoggerCallback(
                  api_key=os.environ.get('WANDB_API_KEY'),
                  project="BugsPlus",
                  group="dqn",
                  job_type="train",
+                 entity="bugplus",
              ),
-         ],
-         verbose=0,
-         checkpoint_freq=10,
-         checkpoint_at_end=True,
-         keep_checkpoints_num=5,
-         )
+],
+    verbose=0,
+    checkpoint_freq=10,
+    checkpoint_at_end=True,
+    keep_checkpoints_num=5,
+)
