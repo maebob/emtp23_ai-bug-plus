afad208d480dfde234ebd73de0645d39e46a31b3
Aaron9812 <79525803+Aaron9812@users.noreply.github.com>
2023-03-04 12:16:16 +0100

    working

--- src/agent_dqn/dqn_tune_rllib.py
+++ src/agent_dqn/dqn_tune_rllib.py
@@ -11,14 +11,16 @@ load_dotenv()
 # append the absolute_project_path from .env variable to the sys.path
 sys.path.append(os.environ.get('absolute_project_path'))
 # from src.environment import environment_ray as environment
-from src.environment.environment_ray_functioning import BugPlus
-from src.utils.matrix import number_bugs, array_to_matrices
+from src.environment.env_complex_observation import BugPlus
+
 
 import ray
 from ray import tune
 import ray.rllib.algorithms.ppo
 import ray.rllib.algorithms.dqn
 
+# clear the terminal
+os.system('clear')
 ray.init()
 
 tune.run("DQN",
