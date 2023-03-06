366294f12801a24a7e15573b2f7a9cdad6432134
Aaron9812 <79525803+Aaron9812@users.noreply.github.com>
2023-03-06 18:33:55 +0100

    Small fixes and test

--- src/engine/test_configs.py
+++ src/engine/test_configs.py
@@ -3,8 +3,17 @@ import json
 import random
 import re
 
+import sys
+import os
+from dotenv import load_dotenv
+
+# load the .env file
+load_dotenv()
+# append the absolute_project_path from .env variable to the sys.path
+sys.path.append(os.environ.get('absolute_project_path'))
+
 from typing import List
-from eval import main as eval_engine
+from src.engine.eval import main as eval_engine
 
 
 def read_config(file: str) -> dict:
@@ -58,8 +67,8 @@ def test_config(formula: str, config: dict) -> bool:
     # Test with 5 diffrent inputs
     for i in range(5):
         # generate random inputs
-        x = random.randint(1, 5)
-        y = random.randint(1, 5)
+        x = random.randint(3, 8)
+        y = random.randint(3, 8)
 
         # calculate the result with the formula
         formula, result = calculate_result(formula, x, y)
@@ -68,14 +77,25 @@ def test_config(formula: str, config: dict) -> bool:
         config["xValue"] = x
         config["yValue"] = y
 
-        engine_result = eval_engine(config).get("0_Out")
+        try:
+            engine_result = eval_engine(config).get("0_Out")
+        except:
+            print("*** ERROR ***")
+            print("Failed: " + formula)
+            print(f"Inputs: x={x}, y={y}")
+            print(f"Result: {result}")
+            print("Engine Result: None")
+            print("\n"*3)
+            break
 
         # check if the result is incorrect
         if result != engine_result:
+            print("*** FAILED ***")
             print("Failed: " + formula)
             print(f"Inputs: x={x}, y={y}")
             print(f"Result: {result}")
             print(f"Engine Result: {engine_result}")
+            print("\n"*3)
             break
 
 
