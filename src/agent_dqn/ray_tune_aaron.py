fb6232522d2f8bb61bdaa4168db36d89b7fffad3
Aaron9812 <79525803+Aaron9812@users.noreply.github.com>
2023-03-06 19:53:00 +0100

    Own training

---
+++ src/agent_dqn/ray_tune_aaron.py
@@ -0,0 +1,61 @@
+# from ray.tune.integration.wandb import WandbLoggerCallback
+from ray.air.integrations.wandb import WandbLoggerCallback
+from ray.tune.stopper import CombinedStopper, ExperimentPlateauStopper, MaximumIterationStopper
+import ray.rllib.algorithms.dqn
+import ray.rllib.algorithms.ppo
+import pandas as pd
+import ray
+from ray import tune
+
+import sys
+import os
+from dotenv import load_dotenv
+
+
+# load the .env file
+load_dotenv()
+# append the absolute_project_path from .env variable to the sys.path
+sys.path.append(os.environ.get('absolute_project_path'))
+from src.environment.env_remove_edge import BugPlus
+
+
+# clear the terminal
+os.system('clear')
+ray.init()
+
+# implement erarly stopping based on the mean reward
+stop = {
+    "episode_reward_mean": 100,
+    "timesteps_total": 100000,
+}
+
+# TODO: test for hyperparameter tuning: config before tune.run with grid_search
+# 'parameter_name': tune.grid_search([True, False])
+
+tune.run("DQN", 
+        config={"env": BugPlus, 
+            "seed": 42069,
+            "framework": "torch",
+            "num_workers": 2, # TODO: anpassen
+            "num_gpus": 0,
+            "num_envs_per_worker": 1,
+            "num_cpus_per_worker": 1,
+        },
+         local_dir="result_data/",
+         callbacks=[
+             WandbLoggerCallback(
+                 api_key=os.environ.get('WANDB_API_KEY'),
+                 project="BugsPlus",
+                 group="dqn_remove_edge",
+                 job_type="train",
+                 entity="bugplus",
+             ),
+],
+    verbose=0,
+    checkpoint_freq=10,
+    checkpoint_at_end=True,
+    keep_checkpoints_num=5,
+)
+# Warning message from run:
+# Current log_level is WARN.
+# For more information, set 'log_level': 'INFO' / 'DEBUG' or use the -v and -vv flags.
