be0a80f8abe379a2fea3bf7257236b2f3d0c473a
Miss Sophie <95981851+misssophieexplores@users.noreply.github.com>
2023-03-10 14:51:08 +0100

    Revert "run agent with sparse rewards, empty matrix"
    This reverts commit 010ae91326121277c86ad668e0cab9ca46c26cad.

--- src/environment/env_complex_observation.py
+++ src/environment/env_complex_observation.py
@@ -105,13 +105,11 @@ class BugPlus(Env):
         if self.epsiode_length > 100: #30:
             self.done = True
             truncated = True
-            reward = -1
-            return self.state, reward, self.done, truncated, {'ep_return': self.ep_return}
+            return self.state, -1, self.done, truncated, {'ep_return': self.ep_return}
         
         if self.state.get("matrix")[action] == 1:
             # The action was already performed, punish the agent
-            # reward = -0.2
-            reward = 0 # sparse reward
+            reward = -0.2
             done = False
             truncated = False
             return self.state, reward, done, truncated, {'ep_return': self.ep_return}
@@ -153,27 +151,23 @@ class BugPlus(Env):
             result = eval_engine(matrix_as_json)
         except TimeoutError:
             # The engine timed out, the bug is invalid likely a loop
-            # reward = -10
-            reward = -2 # sparse reward
+            reward = -30 #-10 #TODO: change back
             done = True
             return reward, done
         except:
             # If the bug is not valid, the engine will throw an error
             # something in the control flow is not connected (but not a loop), execution cannot terminate
-            # reward = -0.1
-            reward = 0 # sparse reward
+            reward = -0.1
             done = False
             
             return reward, done
         if result.get("0_Out") == self.state.get("output"):
             # If the result is correct, the reward is 100
-            # reward = 100
-            reward = 1 # sparse reward
+            reward = 100
             done = True
             return reward, done
         # Engine evaluated but result was not correct
-        # reward = -0.1
-        reward = 0 # sparse reward
+        reward = -0.1
         done = False
         return reward, done
 
