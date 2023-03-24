216ad51177332d5ee19f24bd4c57893860bf62fd
maebob <55137042+maebob@users.noreply.github.com>
2023-03-24 14:17:32 +0100

    data flow errors

--- src/utils/error_to_action.py
+++ src/utils/error_to_action.py
@@ -23,7 +23,22 @@ def translate(error: dict, no_bugs: int) -> np.ndarray:
     # Check whether the passed error concerns the control flow or the data flow
     if error['port'] == 'Up' or error['port'] == 'Down' or error['port'] == 'Out':
         # DATA FLOW
-        pass
+        # Calculate column index
+        if error['bug'] == 0:
+            index_col = 0
+        else:
+            index_col = 1 + int(error['bug'])
+
+        index_row = 0
+        while index_row <= (no_bugs + 1):
+            indices_missing_edges.append([index_col, index_row])
+            index_row += 1
+        
+        # Translate indices into the corresponding actions used to place the edges
+        for index_pair in indices_missing_edges:
+            
+            actions = np.append(actions, (index_pair[1] + (2 * no_bugs) + index_pair[0] + 35))
+
     else:
         # CONTROL FLOW
         # Calculate column index
