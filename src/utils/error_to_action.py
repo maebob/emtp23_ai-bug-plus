6a3fef8c1bbcbfab56ec58afbaadea20c79eb60d
Miss Sophie <95981851+misssophieexplores@users.noreply.github.com>
2023-04-02 12:21:57 +0200

    added translation suggestion and test caases for errors

--- src/utils/error_to_action.py
+++ src/utils/error_to_action.py
@@ -36,8 +36,18 @@ def translate(error: dict, no_bugs: int) -> np.ndarray:
         
         # Translate indices into the corresponding actions used to place the edges
         for index_pair in indices_missing_edges:
-            
-            actions.append(index_pair[1] * (2 * no_bugs) + index_pair[0] + 35) 
+
+            # TRANSLATION:
+            # für 3 bugs:
+            # (Zeile * 5) + Spalte + 35
+
+            # für n bugs:
+            # (Zeile * (no_bugs + 2 )) + Spalte + size_matrix((no_bugs + 2) * (no_bugs + 2))
+            # index_row * (no_bugs + 2) + index_col + ((no_bugs + 2) * (no_bugs + 2))
+            # (index_pair[1] * (no_bugs + 2) + index_pair[0] + ((no_bugs + 2) * (no_bugs + 2))
+            # !!!! (Spalte, Zeile) !!!!  -> Konvention für Position in Matrizen: (Zeile, Spalte)
+
+            actions.append(index_pair[1] * (2 * no_bugs) + index_pair[0] + 35)
 
     else:
         # CONTROL FLOW
@@ -61,7 +71,28 @@ def translate(error: dict, no_bugs: int) -> np.ndarray:
     return actions
 
 if __name__ == '__main__':
-    error = {'port': 'Right', 'bug': 2}
+    print("error = {'port': 'Left', 'bug': 1}")
+    error = {'port': 'Left', 'bug': 1}
     no_bugs = 3
     actions = translate(error, no_bugs)
     print(actions)
+
+    print("\nerror_4 = {'port': 'Up', 'bug': 1}")
+    error_4 = {'port': 'Up', 'bug': 1}
+    actions_4 = translate(error_4, no_bugs)
+    print(actions_4)
+
+    print("\nerror_5 = {'port': 'Out', 'bug': 1}")
+    error_5 = {'port': 'Out', 'bug': 1}
+    actions_5 = translate(error_5, no_bugs)
+    print(actions_5)
+
+    print("\nerror_6 = {'port': 'Out', 'bug': 0}")
+    error_6 = {'port': 'Out', 'bug': 0}
+    actions_6 = translate(error_6, no_bugs)
+    print(actions_6)
+
+    print("\nerror_7 = {'port': 'Up', 'bug': 0}")
+    error_7 = {'port': 'Up', 'bug': 0}
+    actions_7 = translate(error_7, no_bugs)
+    print(actions_7)
